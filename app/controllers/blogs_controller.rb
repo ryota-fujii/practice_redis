53b11f7ed418f35613d1941d5bf83903802dc47d
藤井良多 <rs7328ry@gmail.com>
2019-04-10 13:02:56 +0900

    first commit

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,14 @@
+class BlogsController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+    
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,11 @@
+PracticeRedis::Application.config.session_store :redis_store, {
+    servers: [
+        {
+        host: "localhost", 
+        port: 6379,
+        db: 0,
+        namespace: "session"
+        },
+    ],
+    expire_after: 90.minutes
+}
